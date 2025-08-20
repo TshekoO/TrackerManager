3035e7c93ce6b90e36455d6a5fb8fdc0ffb6bfb3
TshekoO <tshekoogotlhe101@gmail.com>
2025-08-20 22:22:04 +0200

    task works

--- task-manager-angular/src/app/services/task.service.ts
+++ task-manager-angular/src/app/services/task.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Subject } from 'rxjs';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class TaskService {
+  private apiUrl = '/api/tasks'; // Angular will proxy this to .NET backend
+
   private reloadSubject = new Subject<void>();
   reload$ = this.reloadSubject.asObservable();
 
-  triggerReload() {
-    this.reloadSubject.next();
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
   }
+
+  createTask(task: { title: string; priority: string }): Observable<any> {
+    return this.http.post(this.apiUrl, task);
+  }
+
+  completeTask(id: number): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, {});
+  }
+
+    triggerReload(): void {
+      this.reloadSubject.next();
+    }
 }
